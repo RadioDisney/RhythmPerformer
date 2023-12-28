c34256fa2b4d024c4a69d284ba8628fb732f001f
RadioDisney <xunxun.hu.g@gmail.com>
2023-12-28 10:03:39 +0800

    - 加入蓝牙客户端

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,26 @@
 #include <SdFat.h>
 #include <MD_MIDIFile.h>
 
+#include <Arduino.h>
+#include <BLEMIDI_Transport.h>
+
+#include <hardware/BLEMIDI_Client_ESP32.h>
+
+BLEMIDI_CREATE_DEFAULT_INSTANCE(); //Connect to first server found
+
+//BLEMIDI_CREATE_INSTANCE("",MIDI)                  //Connect to the first server found
+//BLEMIDI_CREATE_INSTANCE("f2:c1:d9:36:e7:6b",MIDI) //Connect to a specific BLE address server
+//BLEMIDI_CREATE_INSTANCE("MyBLEserver",MIDI)       //Connect to a specific name server
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 2 //modify for match with yout board
+#endif
+
+void ReadCB(void *parameter);       //Continuos Read function (See FreeRTOS multitasks)
+
+unsigned long t0 = millis();
+bool isConnected = false;
+
 #define USE_MIDI  1  // set to 1 for MIDI output, 0 for debug output
 
 #if USE_MIDI // set up for direct MIDI serial output
@@ -63,6 +83,51 @@ void midiCallback(midi_event *pev)
   }
 }
 
+void initBLEMIDI()
+{
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+
+  BLEMIDI.setHandleConnected([]()
+                             {
+                              //  Serial.println("---------CONNECTED---------");
+                               isConnected = true;
+                               digitalWrite(LED_BUILTIN, HIGH);
+                             });
+
+  BLEMIDI.setHandleDisconnected([]()
+                                {
+                                  // Serial.println("---------NOT CONNECTED---------");
+                                  isConnected = false;
+                                  digitalWrite(LED_BUILTIN, LOW);
+                                });
+
+  MIDI.setHandleNoteOn([](byte channel, byte note, byte velocity)
+                       {
+                         Serial.write(0x91);
+                         Serial.write(note);
+                         Serial.write(velocity);
+                         digitalWrite(LED_BUILTIN, LOW);
+                       });
+  MIDI.setHandleNoteOff([](byte channel, byte note, byte velocity)
+                        {
+                         Serial.write(0x81);
+                         Serial.write(note);
+                         Serial.write(velocity);
+                         digitalWrite(LED_BUILTIN, HIGH);
+                        });
+
+  xTaskCreatePinnedToCore(ReadCB,           //See FreeRTOS for more multitask info  
+                          "MIDI-READ",
+                          3000,
+                          NULL,
+                          1,
+                          NULL,
+                          1); //Core0 or Core1
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+}
+
 void setup(void)
 {
   int  err;
@@ -77,6 +142,8 @@ void setup(void)
     DEBUGS("\nSD init fail!");
     while (true) ;
   }
+
+  initBLEMIDI();
 
   // Initialize MIDIFile
   SMF.begin(&SD);
@@ -95,9 +162,37 @@ void setup(void)
 
 void loop(void)
 {
+  // if (isConnected && (millis() - t0) > 1000)
+  // {
+  //   t0 = millis();
+
+  //   vTaskDelay(250/portTICK_PERIOD_MS);
+  // }
+
   // play the file
   if (!SMF.isEOF())
   {
     SMF.getNextEvent();
   }
+}
+
+
+/**
+ * This function is called by xTaskCreatePinnedToCore() to perform a multitask execution.
+ * In this task, read() is called every millisecond (approx.).
+ * read() function performs connection, reconnection and scan-BLE functions.
+ * Call read() method repeatedly to perform a successfull connection with the server 
+ * in case connection is lost.
+*/
+void ReadCB(void *parameter)
+{
+//  Serial.print("READ Task is started on core: ");
+//  Serial.println(xPortGetCoreID());
+  for (;;)
+  {
+    MIDI.read(); 
+    vTaskDelay(1 / portTICK_PERIOD_MS); //Feed the watchdog of FreeRTOS.
+    //Serial.println(uxTaskGetStackHighWaterMark(NULL)); //Only for debug. You can see the watermark of the free resources assigned by the xTaskCreatePinnedToCore() function.
+  }
+  vTaskDelay(1);
 }
